72e34d6a1752addaf214b84e08644b320666516a
Stefan Kuntz <Stefan.github@gmail.com>
2018-10-02 22:01:49 +0200

    fix install

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 add-apt-repository universe
 apt-get update
-apt-get install python3-dialog
+apt-get install --yes python3-dialog
 sed -i '/preserve_hostname: false/c\preserve_hostname: true' /etc/cloud/cloud.cfg
