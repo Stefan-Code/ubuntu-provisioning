61e92275924564435c2e5b1d51c121921441fd82
Stefan Kuntz <Stefan.github@gmail.com>
2018-10-02 22:07:33 +0200

    setup root check and locale gen

--- setup.sh
+++ setup.sh
@@ -1,3 +1,13 @@
+#!/bin/bash
+if [[ $EUID -ne 0 ]]; then
+   echo "You are not root!"
+   exit 1
+fi
+
+export LC_ALL="en_US.UTF-8"
+export LC_CTYPE="en_US.UTF-8"
+dpkg-reconfigure locales
+
 add-apt-repository universe
 apt-get update
 apt-get install --yes python3-dialog
